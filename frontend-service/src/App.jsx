dd747bd82fed25959b874168c48f0348bf4d9cd5
Zeina Wady <zeinawady@gmail.com>
2026-05-03 22:09:26 +0300

    Add Sidebar

--- frontend-service/src/App.jsx
+++ frontend-service/src/App.jsx
@@ -1,15 +1,25 @@
-import { BrowserRouter } from 'react-router-dom'
 import './App.css'
 import Sidebar from './components/common/Sidebar'
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
+    
 
+     const userRole = "customer";
 
   return (
-    <BrowserRouter>
-    <Sidebar role={"agent"}></Sidebar>
-    </BrowserRouter>
-  )
+    <div className="d-flex">
+      <Sidebar role={userRole} />
+
+      <main className="flex-grow-1 p-4 bg-white">
+        <Routes>
+          <Route path="/customer/tickets" element={<div>My Tickets Page</div>} />
+          <Route path="/customer/notifications" element={<div>Notifications Page</div>} />
+        </Routes>
+      </main>
+    </div>
+      
+  );
 }
 
-export default App
+export default App;
