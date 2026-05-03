4d4da4d3c4cc470d894b19377f12ae6c6bc1e091
Zeina Wady <zeinawady@gmail.com>
2026-05-03 18:23:15 +0300

    Resolve conflict

--- frontend-service/src/App.jsx
+++ frontend-service/src/App.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter } from 'react-router-dom'
 import './App.css'
+import Sidebar from './components/common/Sidebar'
 
 function App() {
 
 
   return (
     <BrowserRouter>
+    <Sidebar role={"agent"}></Sidebar>
     </BrowserRouter>
   )
 }
