3ae5a8869b8063e91c4b2f27a94136c356c10bd5
Habiba Abdelgowad Habiba-Adel <habibaabdelgowad9@gmail.com>
2026-05-04 18:01:18 +0300

    testing that docker compose working and all servcies running done

--- support-service/src/app.js
+++ support-service/src/app.js
@@ -10,7 +10,7 @@ const app = express()
 const PORT =process.env.PORT || 3002
 app.use(express.json());
 
-const mongoURI = process.env.MONGO_URI;
+const mongoURI = process.env.MONGODB_URI;
 
 mongoose.connect(mongoURI)
     .then(() => console.log("Connected to MongoDB Atlas successfully!"))
