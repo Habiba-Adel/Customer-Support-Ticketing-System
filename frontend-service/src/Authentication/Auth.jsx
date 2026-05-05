168794b7e25abfa56e1796a9faadc02d67ab467a
Zeina Wady <zeinawady@gmail.com>
2026-05-05 22:36:55 +0300

    resolve conflicts

--- frontend-service/src/Authentication/Auth.jsx
+++ frontend-service/src/Authentication/Auth.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react';
 import styles from './Auth.module.css';
 import { useNavigate } from 'react-router-dom';
+import { loginUser , registerUser } from '../api';
 
 
 export default function Auth({onLogin}) {
